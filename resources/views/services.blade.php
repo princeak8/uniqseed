f76f60b43bdf04e188e9414c318d23fcf9624d5b
michaels042 <mnwachukwu042@gmail.com>
2022-02-01 14:08:45 +0100

    first commit

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -2,8 +2,134 @@
 
     @section('content')
 
-    <div style="height: 100px; width:100%; padding-left: 5%">
+    <div id="colorlib-main">
+			<div class="colorlib-services">
+				<div class="colorlib-narrow-content">
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+							<span class="heading-meta">Services</span>
+							<h2 class="colorlib-heading">Here are some of our expertise</h2>
+						</div>
+					</div>
+					<div class="row row-bottom-padded-md">
+						<div class="col-md-6">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="colorlib-feature animate-box" data-animate-effect="fadeInLeft">
+										<div class="colorlib-icon">
+											<i class="flaticon-worker"></i>
+										</div>
+										<div class="colorlib-text">
+											<h3>General Conctructing</h3>
+											<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+										</div>
+									</div>
+
+									<div class="colorlib-feature animate-box" data-animate-effect="fadeInLeft">
+										<div class="colorlib-icon">
+											<i class="flaticon-sketch"></i>
+										</div>
+										<div class="colorlib-text">
+											<h3>Pre-Contruction Design</h3>
+											<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="colorlib-feature animate-box" data-animate-effect="fadeInLeft">
+										<div class="colorlib-icon">
+											<i class="flaticon-engineering"></i>
+										</div>
+										<div class="colorlib-text">
+											<h3>Building &amp; Modeling</h3>
+											<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+										</div>
+									</div>
+
+									<div class="colorlib-feature animate-box" data-animate-effect="fadeInLeft">
+										<div class="colorlib-icon">
+											<i class="flaticon-crane"></i>
+										</div>
+										<div class="colorlib-text">
+											<h3>Construction Management</h3>
+											<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4">
+							<a href="services.html" class="services-wrap animate-box" data-animate-effect="fadeInRight">
+								<div class="services-img" style="background-image: url(images/services-1.jpg)"></div>
+								<div class="desc">
+									<h3>Design &amp; Build</h3>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-4">
+							<a href="services.html" class="services-wrap animate-box" data-animate-effect="fadeInRight">
+								<div class="services-img" style="background-image: url(images/services-2.jpg)"></div>
+								<div class="desc">
+									<h3>House Remodeling</h3>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-4">
+							<a href="services.html" class="services-wrap animate-box" data-animate-effect="fadeInRight">
+								<div class="services-img" style="background-image: url(images/services-3.jpg)"></div>
+								<div class="desc">
+									<h3>Construction Management</h3>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-4">
+							<a href="services.html" class="services-wrap animate-box" data-animate-effect="fadeInRight">
+								<div class="services-img" style="background-image: url(images/services-4.jpg)"></div>
+								<div class="desc">
+									<h3>Painting &amp; Tiling</h3>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-4">
+							<a href="services.html" class="services-wrap animate-box" data-animate-effect="fadeInRight">
+								<div class="services-img" style="background-image: url(images/services-5.jpg)"></div>
+								<div class="desc">
+									<h3>Kitchen Remodeling</h3>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="get-in-touch" class="colorlib-bg-color">
+				<div class="colorlib-narrow-content">
+					<div class="row">
+						<div class="col-md-6 animate-box" data-animate-effect="fadeInLeft">
+							<h2>Get in Touch!</h2>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+							<p class="colorlib-lead">Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<p><a href="#" class="btn btn-primary btn-learn">Contact me!</a></p>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+
+    <!-- <div style="height: 100px; width:100%; padding-left: 5%">
         Services Content Goes here
-    </div>
+    </div> -->
 
     @endsection
