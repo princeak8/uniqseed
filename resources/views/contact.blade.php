8890952a57d0fa040e32ddac4b590df05fe7c321
michaels042 <mnwachukwu042@gmail.com>
2022-02-04 07:53:59 +0100

    contact form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,7 +16,7 @@
 						<div class="col-md-5">
 							<div class="colorlib-feature colorlib-feature-sm animate-box" data-animate-effect="fadeInLeft">
 								<div class="colorlib-icon">
-									<i class="icon-globe-outline"></i>
+									<i class="icon-globe-outline" style="background-color: #78608B;border-radius: 5px;"></i>
 								</div>
 								<div class="colorlib-text">
 									<p><a href="#">info@domain.com</a></p>
@@ -25,7 +25,7 @@
 
 							<div class="colorlib-feature colorlib-feature-sm animate-box" data-animate-effect="fadeInLeft">
 								<div class="colorlib-icon">
-									<i class="icon-map"></i>
+									<i class="icon-map" style="background-color: #78608B;border-radius: 5px;"></i>
 								</div>
 								<div class="colorlib-text">
 									<p>198 West 21th Street, Suite 721 New York NY 10016</p>
@@ -34,7 +34,7 @@
 
 							<div class="colorlib-feature colorlib-feature-sm animate-box" data-animate-effect="fadeInLeft">
 								<div class="colorlib-icon">
-									<i class="icon-phone"></i>
+									<i class="icon-phone" style="background-color: #78608B;border-radius: 5px;"></i>
 								</div>
 								<div class="colorlib-text">
 									<p><a href="tel://">+123 456 7890</a></p>
@@ -57,8 +57,8 @@
                                         <textarea name="message" id="message" cols="30" rows="7" class="form-control" placeholder="Message" required></textarea>
                                     </div>
                                     <div class="form-group">
-                                        <input type="submit" class="btn btn-primary btn-send-message" style="background-color: #78608B; border: solid 3px #baaabb;"
-                                        value="Send Message">
+                                        <input type="submit" class="btn btn-primary btn-send-message" style="background-color: #78608B; 
+                                        border-radius: 10px; border: solid 3px #baaabb;" value="Send Message">
                                     </div>
                                     
                                 {{ Form::close() }}
