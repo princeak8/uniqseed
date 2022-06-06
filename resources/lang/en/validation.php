43779e6f9e81cdc11f0c61f0446092f805db54c5
princeak <akalodave@gmail.com>
2022-06-06 18:21:52 +0100

    change

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -143,7 +143,7 @@ return [
     'custom' => [
         'captcha' => [
             'required' => 'Captcha is required',
-            'captcha' => 'Wrong Captcha Val',
+            'captcha' => 'Wrong Captcha Value',
         ],
     ],
 
